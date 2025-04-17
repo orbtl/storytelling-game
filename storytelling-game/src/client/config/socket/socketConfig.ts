757cf07ce37d8cda8e2a1c6da93c9f4e34a333b7
Jake Sklarew <twinsendude@gmail.com>
2025-04-16 19:11:32 -0700

    Adjusted websocket url to use wss instead of ws

--- storytelling-game/src/client/config/socket/socketConfig.ts
+++ storytelling-game/src/client/config/socket/socketConfig.ts
@@ -1,4 +1,4 @@
 export const socketPort = '8080';
-export const socketPrefix = 'ws://';
+export const socketPrefix = 'wss://';
 
 export const socketUri = `${socketPrefix}${window.location.hostname}:${socketPort}`;
